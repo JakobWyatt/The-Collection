eb565ef3a01e74a7ec76d6df77911412db8965d6
Jakob Wyatt <jakob.wyatt@gmail.com>
2018-06-06 21:32:25 +0800

    added testing for the new shiny function application matrix operation

--- test/performance_test.cpp
+++ test/performance_test.cpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -7,6 +8,12 @@
 #include "../include/tc/matrix_view.hpp"
 #include "../include/tc/matrix_ops.hpp"
 #include "../include/tc/math.hpp"
+#include "../include/tc/matrix_ops_f.hpp"
+
+template<typename T>
+std::vector<T> underlying_view_data(tc::matrix_view::matrix_view<T> view) {
+    return std::vector<T>(view.data(), view.data() + view.size());
+}
 
 int main () {
     std::vector<double>::size_type test_matrix_height = 5000;
@@ -17,12 +24,25 @@ int main () {
 
     tc::matrix_view::matrix_view<double> matrix(data.data(), test_matrix_height, test_matrix_width);
 
+    auto output1_data = data;
+    auto output2_data = data;
+    tc::matrix_view::matrix_view<double> output1(output1_data.data(), test_matrix_height, test_matrix_width);
+    tc::matrix_view::matrix_view<double> output2(output2_data.data(), test_matrix_height, test_matrix_width);
+
     auto before_slow_test = std::chrono::steady_clock::now();
-    tc::matrix_ops::m_fn(matrix, matrix, tc::math::sigmoid<double>);
+    tc::matrix_ops::m_fn(matrix, output1, tc::math::sigmoid<double>);
     auto after_slow_test = std::chrono::steady_clock::now();
     std::chrono::milliseconds slow_time_taken = std::chrono::duration_cast<std::chrono::milliseconds>(after_slow_test - before_slow_test);
 
+    auto before_fast_test = std::chrono::steady_clock::now();
+    tc::matrix_ops_f::m_fn(matrix, output2, tc::math::sigmoid<double>);
+    auto after_fast_test = std::chrono::steady_clock::now();
+    std::chrono::milliseconds fast_time_taken = std::chrono::duration_cast<std::chrono::milliseconds>(after_fast_test - before_fast_test);
+
+    assert(underlying_view_data(output1) == underlying_view_data(output2));
+
     std::cout << "Slow testing took: " << slow_time_taken.count() << " ms\n";
+    std::cout << "Fast testing took: " << fast_time_taken.count() << " ms\n";
 
     return 0;
 }
