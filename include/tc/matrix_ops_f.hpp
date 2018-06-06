11f4ae5e529e87bb2c95311653280a641774dfed
Jakob Wyatt <jakob.wyatt@gmail.com>
2018-06-06 22:47:30 +0800

    added fast matrix copy

--- include/tc/matrix_ops_f.hpp
+++ include/tc/matrix_ops_f.hpp
@@ -4,11 +4,23 @@
 	#include <cassert>		// assert
 #endif
 #include <cstddef>			// std::size_t
-#include <algorithm>		// std::transform
+#include <algorithm>		// std::transform, std::copy
 #include <execution>		// std::execution::par_unseq
 
 namespace tc {
 	namespace matrix_ops_f {
+		
+		// Matrix elementwise copy.
+		template<typename SizeType = std::size_t, class InputMatrix, class OutputMatrix>
+		void m_cpy(InputMatrix const& in, OutputMatrix& out)
+		{
+			#ifdef _DEBUG
+				assert(in.rows() == out.rows());
+				assert(in.columns() == out.columns());
+			#endif
+			
+			std::copy(std::excecution::par_unseq, in.data(), in.data() + in.size(), out.data());
+		}
 		
 		// Transforms each matrix element with a function.
 		template<typename SizeType = std::size_t, class InputMatrix, class OutputMatrix, typename Function>
