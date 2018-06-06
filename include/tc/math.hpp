8ed6bc39525b80de6ff1b4bff7cad9c0dd71596f
Reece Jones (DESKTOP UNT4J9U) <reece.jones131@gmail.com>
2018-06-06 14:33:55 +0800

    Added sigmoid_deriv function template.

--- include/tc/math.hpp
+++ include/tc/math.hpp
@@ -15,5 +15,15 @@ namespace tc {
 			return ex / (ex + T{1});
 		}
 		
+		// Derivative of the sigmoid function (e^x / (e^x + 1)^2)
+		template<typename T>
+		std::enable_if_t<std::is_floating_point_v<T>, T> sigmoid_deriv(T x)
+		{
+			T ex = std::exp(x);
+			T ex_p1 = ex + T{1};
+			
+			return ex / (ex_p1 * ex_p1);
+		}
+		
 	}
 }
