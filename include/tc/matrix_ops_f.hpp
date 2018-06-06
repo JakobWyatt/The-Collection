98e12e19cec4f4e805ad580ed14a75d06c8f3185
Jakob Wyatt <jakob.wyatt@gmail.com>
2018-06-06 21:33:03 +0800

    added fast version of m_fn, speedups of approx 6.3x

--- include/tc/matrix_ops_f.hpp
+++ include/tc/matrix_ops_f.hpp
@@ -1,10 +1,26 @@
 #pragma once
 
+#ifdef _DEBUG
+	#include <cassert>		// assert
+#endif
+#include <cstddef>			// std::size_t
+#include <algorithm>		// std::transform
+#include <execution>		// std::execution::par_unseq
 
 namespace tc {
 	namespace matrix_ops_f {
 		
-		
+		// Transforms each matrix element with a function.
+		template<typename SizeType = std::size_t, class InputMatrix, class OutputMatrix, typename Function>
+		void m_fn(InputMatrix const& in, OutputMatrix& result, Function function)
+		{
+			#ifdef _DEBUG
+				assert(in.rows() == result.rows());
+				assert(in.columns() == result.columns());
+			#endif
+			
+			std::transform(std::execution::par_unseq, in.data(), in.data() + in.size(), result.data(), function);
+		}
 		
 	}
 }
