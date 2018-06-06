0e42bcf32e5c479e9a385e86045c8edc88922d4c
Jakob Wyatt <jakob.wyatt@gmail.com>
2018-06-06 22:40:36 +0800

    cleaned up testing code, made more generic

--- test/performance_test.cpp
+++ test/performance_test.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <algorithm>
 #include <chrono>
+#include <utility>
+#include <cstddef>
 #include "../include/tc/matrix_view.hpp"
 #include "../include/tc/random.hpp"
 #include "../include/tc/matrix_view.hpp"
@@ -15,34 +17,34 @@ std::vector<T> underlying_view_data(tc::matrix_view::matrix_view<T> view) {
     return std::vector<T>(view.data(), view.data() + view.size());
 }
 
+template<typename Function, typename... Args>
+std::chrono::milliseconds time_function (Function function, Args... args) {
+    auto before_test = std::chrono::steady_clock::now();
+    function(args...);
+    auto after_test = std::chrono::steady_clock::now();
+    return std::chrono::duration_cast<std::chrono::milliseconds>(after_test - before_test);
+}
+
 int main () {
     std::vector<double>::size_type test_matrix_height = 5000;
     std::vector<double>::size_type test_matrix_width = 5000;
     std::vector<double> data(test_matrix_height * test_matrix_width);
-
     std::generate(data.begin(), data.end(), tc::random::random_standard_normal<double>);
 
     tc::matrix_view::matrix_view<double> matrix(data.data(), test_matrix_height, test_matrix_width);
 
-    auto output1_data = data;
-    auto output2_data = data;
-    tc::matrix_view::matrix_view<double> output1(output1_data.data(), test_matrix_height, test_matrix_width);
-    tc::matrix_view::matrix_view<double> output2(output2_data.data(), test_matrix_height, test_matrix_width);
+    std::vector<double> output_data_1(test_matrix_height * test_matrix_width);
+    tc::matrix_view::matrix_view<double> output_1(output_data_1.data(), test_matrix_height, test_matrix_width);
+    std::vector<double> output_data_2(test_matrix_height * test_matrix_width);
+    tc::matrix_view::matrix_view<double> output_2(output_data_2.data(), test_matrix_height, test_matrix_width);
 
-    auto before_slow_test = std::chrono::steady_clock::now();
-    tc::matrix_ops::m_fn(matrix, output1, tc::math::sigmoid<double>);
-    auto after_slow_test = std::chrono::steady_clock::now();
-    std::chrono::milliseconds slow_time_taken = std::chrono::duration_cast<std::chrono::milliseconds>(after_slow_test - before_slow_test);
+    std::cout << "Slow function application: " <<
+        time_function(tc::matrix_ops::m_fn<std::size_t, tc::matrix_view::matrix_view<double>, tc::matrix_view::matrix_view<double>, double(double)>,
+        matrix, output_1, tc::math::sigmoid<double>).count() << " ms\n";
+    std::cout << "Fast function application: " <<
+        time_function(tc::matrix_ops_f::m_fn<std::size_t, tc::matrix_view::matrix_view<double>, tc::matrix_view::matrix_view<double>, double(double)>,
+        matrix, output_2, tc::math::sigmoid<double>).count() << " ms\n";
 
-    auto before_fast_test = std::chrono::steady_clock::now();
-    tc::matrix_ops_f::m_fn(matrix, output2, tc::math::sigmoid<double>);
-    auto after_fast_test = std::chrono::steady_clock::now();
-    std::chrono::milliseconds fast_time_taken = std::chrono::duration_cast<std::chrono::milliseconds>(after_fast_test - before_fast_test);
-
-    assert(underlying_view_data(output1) == underlying_view_data(output2));
-
-    std::cout << "Slow testing took: " << slow_time_taken.count() << " ms\n";
-    std::cout << "Fast testing took: " << fast_time_taken.count() << " ms\n";
-
+    assert(underlying_view_data(output_1) == underlying_view_data(output_2));
     return 0;
 }
